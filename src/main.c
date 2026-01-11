ff9134148017da8e7e25bccb894ca2c5bfbd7cea
byronlove-lace <byronlovelace@proton.me>
2026-01-11 16:39:56 +0000

    feat: add LED functionality

---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include "include/stm32f411xe.h"
+#include <stdint.h>
+
+int main(void) {
+  // Enable clock access to GPIOA
+  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+
+  // Set GPIOA Pin 5 mode to output (01)
+  GPIOA->MODER |= GPIO_MODER_MODER5_0;
+  GPIOA->MODER |= ~GPIO_MODER_MODER5_1;
+
+  // Superloop
+  while (1) {
+    // Set PA5 (LED Pin) to high
+    GPIOA->ODR |= GPIO_ODR_ODR_5;
+  }
+}
