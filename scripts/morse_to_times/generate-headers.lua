71a84bad8248691eaf1bdcd2b96cc6b7801bfcda
byronlove-lace <byronlovelace@proton.me>
2026-01-16 17:49:06 +0000

    chore: clean up pathing, add generate header file and switch storage to txt from csv

---
+++ scripts/morse_to_times/generate-headers.lua
@@ -0,0 +1,69 @@
+local scriptDir = function()
+	local path = debug.getinfo(1, "S").source:match("@(.*[\\/])")
+	return path
+end
+local file_io = require(scriptDir() .. "utils/file_io")
+
+local txtFileToArr = function(file_path)
+	local lines = {}
+	local txt_string = file_io.read(file_path)
+	if txt_string == 1 then
+		print("Failed to read text string for array parsing.")
+		return 1
+	end
+	for line in txt_string:gmatch("([^\n\r]+)") do
+		table.insert(lines, line)
+	end
+	return lines
+end
+
+local genArrHeaders = function(arr)
+	local h_buff = ""
+	h_buff = h_buff .. "#ifndef LED_TIMINGS_H\n"
+	h_buff = h_buff .. "#define LED_TIMINGS_H\n\n"
+	h_buff = h_buff .. "extern const int LED_TIMINGS_SIZE;\n"
+	h_buff = h_buff .. "extern const int led_timings[];\n"
+	h_buff = h_buff .. "#endif // LED_TIMINGS_H\n"
+
+	local c_buff = ""
+	c_buff = c_buff .. '#include "led_timings.h"\n\n'
+	c_buff = c_buff .. "const int LED_TIMINGS_SIZE = " .. #arr .. ";\n"
+	c_buff = c_buff .. "const int led_timings[LED_TIMINGS_SIZE] = {\n"
+	for _, num in ipairs(arr) do
+		c_buff = c_buff .. string.format("%d, \n", num)
+	end
+	c_buff = c_buff .. "};\n\n"
+
+	return h_buff, c_buff
+end
+
+local main = function()
+	local rel_txt_src = "../data/processed/morse_times.txt"
+	local abs_txt_src = scriptDir() .. rel_txt_src
+
+	local rel_h_dest = "../include/led_timings.h"
+	local abs_h_dest = scriptDir() .. rel_h_dest
+	local rel_c_dest = "../src/led_timings.c"
+	local abs_c_dest = scriptDir() .. rel_c_dest
+
+	local morse_times_arr = txtFileToArr(abs_txt_src)
+	if morse_times_arr == 1 then
+		print("Failed to read morse times from " .. rel_txt_src)
+		os.exit(1)
+	end
+	local h_str, c_str = genArrHeaders(morse_times_arr)
+
+	local success = file_io.write(abs_h_dest, h_str)
+	if success == 1 then
+		print("Failed to write h file to path.")
+		os.exit(1)
+	end
+
+	success = file_io.write(abs_c_dest, c_str)
+	if success == 1 then
+		print("Failed to write c file to path.")
+		os.exit(1)
+	end
+end
+
+main()
