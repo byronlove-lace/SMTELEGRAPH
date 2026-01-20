fc54204c061d46e1886ea8b1719df7ed2d36d071
byronlove-lace <byronlovelace@proton.me>
2026-01-20 20:45:24 +0000

    chore: fix typing

--- src/systick.c
+++ src/systick.c
@@ -16,13 +16,13 @@ void systick_msec_delay(uint32_t delay)
   SysTick->VAL = 0; // also sets COUNTFLAG to 0
 
   // AGUG: SYST_CSR (CLKSOURCE)
-  SysTick->CTRL = CTRL_CLKSRC // select internal clocksource and reset the register
+  SysTick->CTRL = CTRL_CLKSRC; // select internal clocksource and reset the register
 
   // AGUG: SYST_CSR (ENABLE)
-  SysTick->CTRL |= CTRL_ENABLE // Enable SysTick
+  SysTick->CTRL |= CTRL_ENABLE; // Enable SysTick
 
-  for (int i = 0; i < delay; i++) {
-    while((SysTick->CTRL & CTRL_COUNTFLAG)) == 0) {} // loop until timer reaches 0
+  for (uint32_t i = 0; i < delay; i++) {
+    while((SysTick->CTRL & CTRL_COUNTFLAG) == 0) {} // loop until timer reaches 0
   }
 
   SysTick->CTRL = 0;
