c8cdbefa3f741162427ca84d4ea6b29a664d4d44
byronlove-lace <byronlovelace@proton.me>
2026-01-22 16:08:57 +0000

    chore: update encoding test to use testing lib

--- scripts/morse_to_times/tests/encoding.test.lua
+++ scripts/morse_to_times/tests/encoding.test.lua
@@ -2,7 +2,7 @@ local json = require("lib/json")
 local morse = require("utils.morse")
 local file_io = require("utils.file_io")
 local str_mod = require("utils/str_mod")
-local tbl = require("utils/tbl")
+local lu = require("lib.luaunit")
 
 -- DATA
 local getScriptDir = function()
@@ -19,15 +19,8 @@ local morse_table = json.decode(json_data)
 -- INPUT - EXPECTATION
 local input = "SOS"
 local input_char = str_mod.strToChar(input)
-local expected_encode = { "...", "---", "..." }
+local expected = { "...", "---", "..." }
 
 -- TEST
-local encode = morse.encode(input_char, morse_table)
-assert(
-	tbl.eq(encode, expected_encode),
-	string.format(
-		"Encode does not match expected output: \nEXPECTED: %s\nENCODE: %s",
-		tbl.dump(expected_encode),
-		tbl.dump(encode)
-	)
-)
+local output = morse.encode(input_char, morse_table)
+lu.assertEquals(output, expected)
